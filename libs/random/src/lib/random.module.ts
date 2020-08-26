3c05372fc3457a9a0efe58a6e4ea9da016542378
Victor Avila <vicav99@gmail.com>
2020-08-25 18:16:08 -0700

    feat: initial scaffolding

---
+++ libs/random/src/lib/random.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RandomComponent } from './random/random.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [RandomComponent],
+})
+export class RandomModule {}
